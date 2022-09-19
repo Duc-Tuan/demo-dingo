f7df86407311ba889e1e132f8f48c3fb624377f0
Duc-Tuan <phamductuan26032002@gmail.com>
2022-09-19 08:39:44 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-import { StateProvider } from "../hook/StateProvider";
-import reducer, {initialState} from "../hook/Reducer";
+// import { StateProvider } from "../hook/StateProvider";
+// import reducer, {initialState} from "../hook/Reducer";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
+    // <StateProvider initialState={initialState} reducer={reducer}>
       <Component {...pageProps} />
-    </StateProvider>
+    // </StateProvider>
   );
 }
 
